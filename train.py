1ce6bdc969be3fc9d245b69d040745caf3aa111a
kwonmha <kwonmha@gmail.com>
2018-05-19 23:26:53 +0900

    add writing vocab, enable to use pre trained word2vec, match dim with pre trained word2vec

--- train.py
+++ train.py
@@ -20,8 +20,8 @@ tf.flags.DEFINE_float("dev_sample_percentage", .1, "Percentage of the training d
 tf.flags.DEFINE_integer("max_sentence_length", 100, "Max sentence length in train(98)/test(70) data (Default: 100)")
 
 # Model Hyperparameters
-tf.flags.DEFINE_string("word2vec", None, "Word2vec file with pre-trained embeddings")
-tf.flags.DEFINE_integer("text_embedding_dim", 100, "Dimensionality of character embedding (Default: 300)")
+tf.flags.DEFINE_string("word2vec", "GoogleNews-vectors-negative300.bin", "Word2vec file with pre-trained embeddings") #
+tf.flags.DEFINE_integer("text_embedding_dim", 300, "Dimensionality of character embedding (Default: 300)")
 # tf.flags.DEFINE_integer("position_embedding_dim", 200, "Dimensionality of position embedding (Default: 100)")
 # tf.flags.DEFINE_string("filter_sizes", "2,3,4,5", "Comma-separated filter sizes (Default: 2,3,4,5)")
 # tf.flags.DEFINE_integer("num_filters", 128, "Number of filters per filter size (Default: 128)")
@@ -34,7 +34,7 @@ tf.flags.DEFINE_integer("n_channels", 100, "the number of channels-output vector
 
 # Training parameters
 tf.flags.DEFINE_integer("batch_size", 64, "Batch Size (Default: 64)")
-tf.flags.DEFINE_integer("num_epochs", 100, "Number of training epochs (Default: 100)")
+tf.flags.DEFINE_integer("num_epochs", 30, "Number of training epochs (Default: 100)") # 100 epochs - 11290 steps
 tf.flags.DEFINE_integer("display_every", 10, "Number of iterations to display training info.")
 tf.flags.DEFINE_integer("evaluate_every", 100, "Evaluate model on dev set after this many steps")
 tf.flags.DEFINE_integer("checkpoint_every", 100, "Save model after this many steps")
@@ -107,7 +107,51 @@ def train():
 					 vocab_size=len(text_vocab_processor.vocabulary_), embedding_size=FLAGS.text_embedding_dim,
 					 f1=FLAGS.f1, f2=FLAGS.f2, n_channels=FLAGS.n_channels)
 
+		# Output directory for models and summaries
+		timestamp = str(int(time.time()))
+		out_dir = os.path.abspath(os.path.join(os.path.curdir, "runs", timestamp))
+		print("Writing to {}\n".format(out_dir))
+
+		# Checkpoint directory. Tensorflow assumes this directory already exists so we need to create it
+		checkpoint_dir = os.path.abspath(os.path.join(out_dir, "checkpoints"))
+		checkpoint_prefix = os.path.join(checkpoint_dir, "model")
+		if not os.path.exists(checkpoint_dir):
+			os.makedirs(checkpoint_dir)
+		saver = tf.train.Saver(tf.global_variables(), max_to_keep=FLAGS.num_checkpoints)
+
+		# Write vocabulary
+		text_vocab_processor.save(os.path.join(out_dir, "text_vocab"))
+		# pos_vocab_processor.save(os.path.join(out_dir, "position_vocab"))
+
 		sess.run(tf.global_variables_initializer())
+
+		# Pre-trained word2vec
+		if FLAGS.word2vec:
+			# initial matrix with random uniform
+			initW = np.random.uniform(-0.25, 0.25, (len(text_vocab_processor.vocabulary_), FLAGS.text_embedding_dim))
+			# load any vectors from the word2vec
+			print("Load word2vec file {0}".format(FLAGS.word2vec))
+			with open(FLAGS.word2vec, "rb") as f:
+				header = f.readline()
+				vocab_size, layer1_size = map(int, header.split())
+				binary_len = np.dtype('float32').itemsize * layer1_size
+				for line in range(vocab_size):
+					word = []
+					while True:
+						ch = f.read(1).decode('latin-1')
+						if ch == ' ':
+							word = ''.join(word)
+							break
+						if ch != '\n':
+							word.append(ch)
+					idx = text_vocab_processor.vocabulary_.get(word)
+					if idx != 0:
+						initW[idx] = np.fromstring(f.read(binary_len), dtype='float32')
+					else:
+						f.read(binary_len)
+			sess.run(model.W_emb.assign(initW))
+			print("Success to load pre-trained word2vec model!\n")
+
 		batches = data_helpers.batch_iter(list(zip(x_train, y_train)), FLAGS.batch_size, FLAGS.num_epochs)
 
 		for step, batch in enumerate(batches):
@@ -134,6 +178,11 @@ def train():
 
 				f1 = f1_score(np.argmax(y_dev, axis=1), predictions, average="macro")
 				print("step {}:, loss {}, acc {}, f1 {}\n".format(step, loss, accuracy, f1))
+
+			# Model checkpoint
+			if step % FLAGS.checkpoint_every == 0:
+				path = saver.save(sess, checkpoint_prefix, global_step=step)
+				print("Saved model checkpoint to {}\n".format(path))
 
 def main(_):
 	train()
